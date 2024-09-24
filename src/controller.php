22278e2010dce8124d084657fcad403fc1ab945e
dev <302685723@student.rocmondriaan.nl>
2024-09-24 13:42:29 +0200

    buildForm

--- src/controller.php
+++ src/controller.php
@@ -1,12 +1,34 @@
 <?php 
 class Controller {
+    /**
+     * Adds the html page to the screen
+     * @param mixed $view
+     * @param array $data
+     * @return void
+     */
     public function render($view, $data = []) {
-        extract($data);
+        extract($data); //turns the array into multiple variables
         include "../src/pages/" . $view;
     }
 
+    /**
+     * Routes the user to another page
+     * @param string $route
+     */
     public function routeToUri(string $route) {
         header("Location: $route");
         return;
+    }
+
+    /**
+     * Creates an form. 
+     * @param string $item
+     * @param mixed $item
+     * @return class
+     */
+    public function buildForm(string $class, $item) {
+        $c = new $class;
+        $c->init();
+        return $c;
     }
 }
