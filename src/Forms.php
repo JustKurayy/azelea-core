920806bc07ffa653376b613afca45a3a0e4cf8a8
dev <302685723@student.rocmondriaan.nl>
2024-09-27 17:22:54 +0200

    form parsing and validating

--- src/Forms.php
+++ src/Forms.php
@@ -2,7 +2,8 @@
 namespace Azelea\Core;
 
 class Forms {
-    private $fieldHtml = array();
+    private $fieldHtml = array(); //stores all the fields, also used for loading them in
+    private $formField; //stores the input field
     private $csrf;
 
     /**
@@ -10,7 +11,7 @@ class Forms {
      * @param string $name
      */
     private function generateCsrf(string $name) {
-        $_SESSION['_csrf'][$name] = bin2hex(random_bytes(32));
+        $_SESSION['_csrf'][$name] = bin2hex(random_bytes(32)); //bin2hex is safer than md5
         $this->csrf = $_SESSION['_csrf'][$name];
         return;
     }
@@ -20,32 +21,26 @@ class Forms {
      * @param array $fields
      */
     public function generateFields(array $fields) {
+        header("X-XSS-Protection: 1; mode=block");
+        header("X-Content-Type-Options: nosniff");
         foreach ($fields as $field) {
             switch ($field["type"]) {
                 case "text":
-                    $f = "<input type='".$field['type']."' name='".$field['name']."' class='".$field['classes']."' id='".$field['name']."'";
+                    $this->formField = "<input type='".$field['type']."' name='".$field['name']."' class='".$field['classes']."' id='".$field['name']."'";
 
-                    if (array_key_exists("options", $field)) { //only removes the label if it is specifically defined not to load
-                        try {
-                            if ($field["options"]["label"] == false) {
-                                array_push($this->fieldHtml, $f);
-                            } else {
-                                $l = "<label id='".$field['name']."'>".$field['name']."</label>";
-                                array_push($this->fieldHtml, $l);
-                            }
-                            if ($field["options"]["required"] == false) {
-                                $f .= "required";
-                            }
-                        } catch (\Exception $e) {
-                            //leaving it empty as no error log is needed here. 
+                    if (array_key_exists("options", $field)) { //checks if the options field even exists
+                        $this->parseFieldOptions($field["options"], $field);
+                        $this->formField .= ">"; //closes the field
+                        if (array_key_exists("wrapped", $field["options"])) { //wraps the field in a div
+                            $this->formField = "<div class='".$field["options"]["wrapped"]."'>" . $this->formField . "</div>";
                         }
                     } else {
                         $l = "<label id='".$field['name']."'>".$field['name']."</label>";
                         array_push($this->fieldHtml, $l);
+                        $this->formField .= "required='required'>";
                     }
 
-                    $f .= ">";
-                    array_push($this->fieldHtml, $f); //always loads the field itself
+                    array_push($this->fieldHtml, $this->formField); //always loads the field itself
                     break;
                 case "label":
                     $l = "<label class='".$field['classes']."'>".$field['name']."</label>";
@@ -66,6 +61,30 @@ class Forms {
     }
 
     /**
+     * Loops through the options field, if it exists, and adjusts the form to that.
+     * Starts with the fallback, then adds existing options.
+     * @param array $options
+     * @param mixed $field
+     */
+    private function parseFieldOptions(array $options, array $form) {
+        //fallback for if the key doesn't exist
+        if (!array_key_exists("required", $options)) $this->formField .= "required='required'";
+        if (!array_key_exists("label", $options)) array_push($this->fieldHtml, "<label id='".$form['name']."'>".$form['name']."</label>");
+
+        //loops through the options and adds them if they exist
+        foreach ($options as $o => $v) {
+            switch ($o) {
+                case "label":
+                    ($v !== false) ? array_push($this->fieldHtml, "<label id='".$form['name']."'>".$form['name']."</label>") : "";
+                    break;
+                case "required":
+                    ($v === false) ? $this->formField .= "" : $this->formField .= "required='required'";
+                    break;   
+            }
+        }
+    }
+
+    /**
      * Shows the form on the page.
      * Argument is passed as html class(es).
      * Argument is optional.
@@ -81,12 +100,13 @@ class Forms {
 
     /**
      * Filters the input data, to prevent sql injection or any other trouble.
-     * Remove ? from ?array, as it should return the data in array for further parsing
-     * @param $data
-     * @return array
+     * Very basic right now. Do NOT rely on it!
+     * @param mixed $data
+     * @return mixed
      */
-    private function filterFields($data): ?array {
-        return null;
+    private function filterField($data) {
+        if (filter_var($data, FILTER_VALIDATE_INT)) return filter_var(trim($data), FILTER_SANITIZE_NUMBER_INT);
+        if (is_string($data)) return htmlspecialchars(trim(strip_tags($data)), ENT_QUOTES, 'UTF-8');
     }
 
     /**
@@ -97,6 +117,7 @@ class Forms {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if ($_POST['_csrf'] === $_SESSION['_csrf'][$this->csrf]) {
                 $_SESSION['_csrf'] = [];
+                //TODO: foreach filter check if valid
                 return true;
             } else {
                 return false;
@@ -110,6 +131,6 @@ class Forms {
      * @param string $id
      */
     public function getData(string $id) {
-        return $_POST[$id]; //temp solution
+        return $this->filterField($_POST[$id]); //double check
     }
 }
