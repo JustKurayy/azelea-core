75c8361564a2ad59aaf71f4b6c199b5ba6ed3dd6
dev <302685723@student.rocmondriaan.nl>
2024-10-11 14:43:49 +0200

    router changes

--- src/Router.php
+++ src/Router.php
@@ -12,22 +12,20 @@ class Router {
     public function addRoute($method, $path, $handler, $args = []) {
         header("X-XSS-Protection: 1; mode=block");
         header("X-Content-Type-Options: nosniff");
-        $routes[] = array('method' => $method, 'path' => $path, 'handler' => $handler);
-        return $this->dispatch("GET", $_SERVER['REQUEST_URI'], $routes, $args);
-    }
-
-    private function dispatch($requestMethod, $requestPath, $routes, $args = []) {
-        foreach ($routes as $route) {
-            if ($route['method'] === $requestMethod && $route['path'] === $requestPath) {
-                $rt = explode("::", $route['handler']); //apparently the namespace wasn't good enough
-                $className = "Azelea\\Core\\".$rt[0];
+        try {
+            if ($method == $_SERVER["REQUEST_METHOD"] && $path == $_SERVER['REQUEST_URI']) {
+                $rt = explode("::", $handler);
+                $className = "Azelea\\Core\\" . $rt[0];
                 $class = new $className;
                 $func = $rt[1];
                 $call = (count($args) === 0) ? $class->$func() : $class->$func($args);
                 return;
+            } else {
+                throw new \Exception("Invalid Route");
+                return;
             }
+        } catch (\Exception $e) {
+            return Core::error($e);
         }
-        //commented because it is unchecked, resulting in it always returning 404
-        return; //http_response_code(404);
     }
 }
